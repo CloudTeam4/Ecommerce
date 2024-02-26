af34a740dc697565105daf01f09616ed11a3a082
JEON <jwodn123@naver.com>
2024-02-26 16:29:42 +0900

    feat: Add Exception Handler,ADD Product function

--- src/main/kotlin/com/teamsparta/ecommerce/EcommerceApplication.kt
+++ src/main/kotlin/com/teamsparta/ecommerce/EcommerceApplication.kt
@@ -3,13 +3,10 @@ package com.teamsparta.ecommerce
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-// test2
 @SpringBootApplication
 class EcommerceApplication
 
 
 fun main(args: Array<String>) {
     runApplication<EcommerceApplication>(*args)
-
-
 }
