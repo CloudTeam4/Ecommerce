ab616eaff76f6625d2a5cd52a8a1bc7f68063ad1
JEON <jwodn123@naver.com>
2024-03-05 16:22:56 +0900

    [REFACTOR] 선착순 쿠폰 발급 API 리팩토링 -> redisson 분산 락 적용 및 redis에 데이터 저장 로직 추가 [TEST] 테스트 코드 작성 중

--- src/main/kotlin/com/teamsparta/ecommerce/config/aaaaa.kt
+++
@@ -1,5 +0,0 @@
-package com.teamsparta.ecommerce.config
-
-class aaaaa {
-    val a = 10
-}
--- src/main/kotlin/com/teamsparta/ecommerce/domain/member/repository/MemberRepository.kt
+++ src/main/kotlin/com/teamsparta/ecommerce/domain/member/repository/MemberRepository.kt
@@ -2,8 +2,10 @@ package com.teamsparta.ecommerce.domain.member.repository
 
 import com.teamsparta.ecommerce.domain.member.model.Member
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import java.util.*
 
+@Repository
 interface MemberRepository : JpaRepository<Member, Long> {
 
     fun existsByEmail (email : String) : Boolean
