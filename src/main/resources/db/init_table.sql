702d911b28d5e0e4f994213b390f6df8aecb66a6
bheejun <bhjun95@gmail.com>
2024-03-18 15:37:47 +0900

    [TEST] 쿠폰 발급 테스트 작성 및 통과 확인

--- src/main/resources/db/init_table.sql
+++ src/main/resources/db/init_table.sql
@@ -1,195 +1,96 @@
-CREATE TABLE IF NOT EXISTS member
+-- Member Table
+CREATE TABLE member
 (
-    id
-               BIGINT
-        AUTO_INCREMENT
-        PRIMARY
-            KEY,
-    email
-               VARCHAR(255) NOT NULL,
-    password   VARCHAR(255) NOT NULL,
-    phone      VARCHAR(255) NOT NULL,
-    address    VARCHAR(255) NOT NULL,
-    nickname   VARCHAR(255) NOT NULL,
-    role       VARCHAR(255) NOT NULL,
-    cart_id    BIGINT,
-    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone    VARCHAR(255) NOT NULL,
+    address  VARCHAR(255) NOT NULL,
+    nickname VARCHAR(255) NOT NULL,
+    role     VARCHAR(255) NOT NULL,
+    cart_id  BIGINT UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS product
+-- Cart Table
+CREATE TABLE cart
 (
-    item_id
-                BIGINT
-        AUTO_INCREMENT
-        PRIMARY
-            KEY,
-    category
-                VARCHAR(255) NOT NULL,
-    name        VARCHAR(255),
+    id             BIGINT AUTO_INCREMENT PRIMARY KEY,
+    total_quantity INT DEFAULT 0
+);
+
+-- CartItem Table
+CREATE TABLE cart_item
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    cart_id    BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity   INT    NOT NULL,
+    FOREIGN KEY (cart_id) REFERENCES cart (id),
+    FOREIGN KEY (product_id) REFERENCES product (item_id)
+);
+
+-- Product Table
+CREATE TABLE product
+(
+    item_id     BIGINT AUTO_INCREMENT PRIMARY KEY,
+    category    VARCHAR(255) NOT NULL,
+    name        VARCHAR(255) NOT NULL,
     explanation TEXT,
     price       INT          NOT NULL,
     image       VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS cart
+-- Coupon Table
+CREATE TABLE coupon
 (
-    id
-        BIGINT
-        AUTO_INCREMENT
-        PRIMARY
-            KEY,
-    total_quantity
-        INT
-        NOT
-            NULL
+    coupon_id   BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_id   BIGINT NOT NULL,
+    name        VARCHAR(255),
+    explanation TEXT,
+    price       INT,
+    status      VARCHAR(255),
+    type        BOOLEAN,
+    statufields VARCHAR(255),
+    quantity    INT,
+    FOREIGN KEY (member_id) REFERENCES member (id)
 );
 
-CREATE TABLE IF NOT EXISTS coupon
+-- CouponBox Table
+CREATE TABLE coupon_box
 (
-    coupon_id
-                   BIGINT
-        AUTO_INCREMENT
-        PRIMARY
-            KEY,
-    member_id
-                   BIGINT
-                             NOT
-                                 NULL,
-    name
-                   VARCHAR(255),
-    explanation    TEXT,
-    deducted_price INT       NOT NULL,
-    status         VARCHAR(255),
-    type           BOOLEAN,
-    applicable     VARCHAR(255),
-    quantity       INT       NOT NULL,
-    created_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY
-        (
-         member_id
-            ) REFERENCES member
-        (
-         id
-            )
+    coupon_box_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_id     BIGINT NOT NULL,
+    coupon_id     BIGINT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (coupon_id) REFERENCES coupon (coupon_id)
 );
 
-CREATE TABLE IF NOT EXISTS orders
+-- Order Table
+CREATE TABLE orders
 (
-    id
-                 BIGINT
-        AUTO_INCREMENT
-        PRIMARY
-            KEY,
-    payment_method
-                 VARCHAR(255) NOT NULL,
-    total_price  INT,
-    status       VARCHAR(255) NOT NULL,
-    order_date   TIMESTAMP    NOT NULL,
-    arrival_date TIMESTAMP,
-    member_id    BIGINT       NOT NULL,
-    FOREIGN KEY
-        (
-         member_id
-            ) REFERENCES member
-        (
-         id
-            )
+    id             BIGINT AUTO_INCREMENT PRIMARY KEY,
+    payment_method VARCHAR(255) NOT NULL,
+    total_price    INT,
+    status         VARCHAR(255) NOT NULL,
+    order_date     TIMESTAMP    NOT NULL,
+    arrival_date   TIMESTAMP,
+    member_id      BIGINT       NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member (id)
 );
 
-CREATE TABLE IF NOT EXISTS coupon_box
+-- OrderDetail Table
+CREATE TABLE order_detail
 (
-    coupon_box_id
-        BIGINT
-        AUTO_INCREMENT
-        PRIMARY
-            KEY,
-    member_id
-        BIGINT
-        NOT
-            NULL,
-    coupon_id
-        BIGINT
-        NOT
-            NULL,
-    FOREIGN
-        KEY
-        (
-         member_id
-            ) REFERENCES member
-        (
-         id
-            ),
-    FOREIGN KEY
-        (
-         coupon_id
-            ) REFERENCES coupon
-        (
-         coupon_id
-            )
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    quantity INT    NOT NULL,
+    order_id BIGINT NOT NULL,
+    item_id  BIGINT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders (id),
+    FOREIGN KEY (item_id) REFERENCES product (item_id)
 );
 
-CREATE TABLE IF NOT EXISTS cart_item
-(
-    id
-        BIGINT
-        AUTO_INCREMENT
-        PRIMARY
-            KEY,
-    cart_id
-        BIGINT,
-    product_id
-        BIGINT,
-    quantity
-        INT
-        NOT
-            NULL,
-    FOREIGN
-        KEY
-        (
-         cart_id
-            ) REFERENCES cart
-        (
-         id
-            ),
-    FOREIGN KEY
-        (
-         product_id
-            ) REFERENCES product
-        (
-         item_id
-            )
-);
-
-CREATE TABLE IF NOT EXISTS order_detail
-(
-    id
-        BIGINT
-        AUTO_INCREMENT
-        PRIMARY
-            KEY,
-    quantity
-        INT
-        NOT
-            NULL,
-    orders_id
-        BIGINT,
-    item_id
-        BIGINT,
-    FOREIGN
-        KEY
-        (orders_id
-            ) REFERENCES orders
-        (
-         id
-            ),
-    FOREIGN KEY
-        (
-         item_id
-            ) REFERENCES product
-        (
-         item_id
-            )
-);
-
+-- Alter Table to add foreign key for Cart in Member
+ALTER TABLE member
+    ADD CONSTRAINT fk_cart
+        FOREIGN KEY (cart_id)
+            REFERENCES cart (id);
--- src/test/kotlin/com/teamsparta/ecommerce/integrationtest/IntegrationTest.kt
+++ src/test/kotlin/com/teamsparta/ecommerce/integrationtest/IntegrationTest.kt
@@ -5,6 +5,7 @@ import com.teamsparta.ecommerce.domain.coupon.repository.CouponBoxRepository
 import com.teamsparta.ecommerce.domain.coupon.repository.CouponRepository
 import com.teamsparta.ecommerce.domain.coupon.service.CouponService
 import com.teamsparta.ecommerce.domain.member.repository.MemberRepository
+import com.teamsparta.ecommerce.domain.member.service.MemberService
 import com.teamsparta.ecommerce.util.rabbit.RabbitService
 import org.junit.jupiter.api.BeforeAll
 import org.redisson.spring.data.connection.RedissonConnectionFactory
@@ -23,8 +24,7 @@ import org.testcontainers.containers.MySQLContainer
 @ActiveProfiles("test") //테스트용 프로파일인 "test"를 활성화, 이 프로파일을 사용하면 테스트용 환경 설정이 로드.
 @SpringBootTest(classes = [EcommerceApplication::class])
 //@Sql("classpath:/db/init_table.sql") //테스트용 데이터베이스의 초기 데이터를 로드하는데 사용, classpath를 통해 클래스패스 상의 SQL 파일을 지정
-//@Sql("classpath:/db/dml.sql") //DML 쿼리를 실행하여 초기 데이터베이스 상태를 설정하는데 사용
-//@Import(RedisTestConfig::class)
+@Sql("classpath:/db/dml.sql") //DML 쿼리를 실행하여 초기 데이터베이스 상태를 설정하는데 사용
 class IntegrationTest() {
     @Autowired
     lateinit var couponService: CouponService
@@ -38,19 +38,22 @@ class IntegrationTest() {
     lateinit var rabbitService: RabbitService
     @Autowired
     lateinit var couponBoxRepository: CouponBoxRepository
+    @Autowired
+    lateinit var memberService: MemberService
 
 
     companion object {
 
 
         @JvmStatic
-        var mysqlContainer: MySQLContainer<*> = MySQLContainer("mysql:latest")
+        var mysqlContainer: MySQLContainer<*> = MySQLContainer("mysql:latest").withExposedPorts(3306)
         @JvmStatic
-        var redisContainer: GenericContainer<*> = GenericContainer("redis:latest")
+        var redisContainer: GenericContainer<*> = GenericContainer("redis:latest").withExposedPorts(6379)
 
         @JvmStatic
         @BeforeAll //JUnit5에서 모든 테스트 메서드 실행 전에 실행될 메서드를 지정, 여기서는 테스트 실행 전에 MySQL 컨테이너를 시작하기 위해 사용
         fun beforeAll() {
+            redisContainer.start()
 
         }
     }
