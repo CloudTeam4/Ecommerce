02c646c70d645b9c4c005eae7828c3243fc2c943
t2023-m0075 <kokosunsun1004@gmail.com>
2024-03-18 13:52:21 +0900

    [FIX] PREMIUM DEAL 이 이벤트 확장성 측면에서 비즈니스 로직이 어색하다고 판단 되어 Event 로 변경

---
+++ src/test/kotlin/com/teamsparta/ecommerce/integrationtest/service/CouponServiceTest.kt
@@ -0,0 +1,49 @@
+import com.teamsparta.ecommerce.domain.coupon.service.CouponService
+import com.teamsparta.ecommerce.integrationtest.IntegrationTest
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+import org.junit.jupiter.api.Assertions.*
+import org.redisson.api.RedissonClient
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.redis.core.RedisTemplate
+
+
+
+@SpringBootTest(classes = [CouponService::class])
+@AutoConfigureMockMvc
+class CouponServiceTest : IntegrationTest() {
+
+    @Autowired
+    lateinit var couponService: CouponService
+    @Autowired
+    lateinit var redissonClient: RedissonClient
+    @Autowired
+    lateinit var redisTemplate: RedisTemplate<String, String>
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun `여러_사용자_선착순_쿠폰_발급`() {
+        val couponId = 1L
+        val threadCount = 1000
+        val executorService = Executors.newFixedThreadPool(32)
+        val countDownLatch = CountDownLatch(threadCount)
+
+        for (i in 0 until threadCount) {
+            val memberId = i.toLong()
+            executorService.submit {
+                try {
+                    couponService.downloadCoupon(memberId, couponId)
+                } finally {
+                    countDownLatch.countDown()
+                }
+            }
+        }
+
+        countDownLatch.await()
+        val count = couponCountRepository.getCount()
+        assertEquals(100, count)
+    }
+}
